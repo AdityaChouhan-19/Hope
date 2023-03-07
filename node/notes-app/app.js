a3532d77a4aa84a7c456ddf5680a03e74e9eb8f2
Aditya Chouhan <104201860+AdityaChouhan-19@users.noreply.github.com>
2023-03-07 04:57:34 -0500

    Cleared code.

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,18 +1,6 @@
-// const addition = require('./utils') 
-// //using exported add function of utils naming for reguire thing can change
+const Notes = require('./notes');
 
-// const sum = addition(3,13)
-// console.log(sum)
-
-// console.log(addition(2,3))
-
-//activity
-
-const NotesFile = require('./notes');
-
-console.log(NotesFile());
-
-//or 
-
-const YourNotes = NotesFile();
+//console.log(Notes());
+//or
+const YourNotes = Notes();
 console.log(YourNotes)
