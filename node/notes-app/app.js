100ff368bac12d33586d969ea180dbc649e87c21
Aditya Chouhan <104201860+AdityaChouhan-19@users.noreply.github.com>
2023-03-07 04:14:06 -0500

    require function for different custom file.

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,11 +1,5 @@
-//you need to require file system module before use!
-const fs = require('fs');
+require('./utils') //you can required another custon file and runnig the main file will execute the code in the other file too.
 
-// format ('name of file', 'content')
-//if file doesn't exit it will be created, if it exists it will be overwritten.
+const name = 'Aditya'
 
-//fs.writeFileSync('notes.txt', 'I am Aditya!');  <-------
-
-//activity
-// appendFileSync() to append content to previously created file.
-fs.appendFileSync('notes.txt', '\nI am trying to learn Node.JS!');
+console.log(name)
