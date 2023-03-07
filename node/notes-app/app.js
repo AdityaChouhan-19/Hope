179e474b5eed5d6dbfc79c076766cdf9cb926071
Aditya Chouhan <104201860+AdityaChouhan-19@users.noreply.github.com>
2023-03-07 04:29:04 -0500

    Activity

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,7 +1,18 @@
-const addition = require('./utils') 
-//using exported add function of utils naming for reguire thing can change
+// const addition = require('./utils') 
+// //using exported add function of utils naming for reguire thing can change
 
-const sum = addition(3,13)
-console.log(sum)
+// const sum = addition(3,13)
+// console.log(sum)
 
-console.log(addition(2,3))
+// console.log(addition(2,3))
+
+//activity
+
+const NotesFile = require('./notes');
+
+console.log(NotesFile());
+
+//or 
+
+const YourNotes = NotesFile();
+console.log(YourNotes)
