8be5792ef384b54d8a2b29f3d76d82720a91a22f
Aditya Chouhan <104201860+AdityaChouhan-19@users.noreply.github.com>
2023-03-07 05:19:45 -0500

    npm setup (validator module)

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,6 +1,15 @@
+//npm init
+//npm i {package name} validator
+
+const validator = require('validator');
+
 const Notes = require('./notes');
 
 //console.log(Notes());
 //or
 const YourNotes = Notes();
-console.log(YourNotes)
+console.log(YourNotes);
+
+console.log(validator.isEmail('aditya@gmail.com')); //returns true if valid and vice-versa; for detailed info check npm website. 
+
+console.log(validator.isUrl('http://google.com'));
