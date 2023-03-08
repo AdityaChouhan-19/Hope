452fbd6acdb0f7fd61e335677649616e843ac9a4
Aditya Chouhan <104201860+AdityaChouhan-19@users.noreply.github.com>
2023-03-08 03:03:02 -0500

    yargs installation

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,12 +1,8 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+
 const notes = require('./notes');
 
-const cmdWord = process.argv[2]; //storing the third item in argv array in variable cmdWord;
 
-if (cmdWord === 'add') {
-    console.log('add...')
-} else if (cmdWord === 'remove') {
-    console.log('remove...')
-}
-
-//use node app add/remove in cmd to see what's happening!
+console.log(process.argv)
+console.log(yargs.argv)//use node app add --title="Aditya"
