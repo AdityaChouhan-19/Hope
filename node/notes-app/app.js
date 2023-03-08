036b28eec2e20a6894f204998d91773cc8a2fecc
Aditya Chouhan <104201860+AdityaChouhan-19@users.noreply.github.com>
2023-03-08 03:20:21 -0500

    yargs "command:"

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -3,6 +3,43 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
+//command keyord in yargs
 
-console.log(process.argv)
-console.log(yargs.argv)//use node app add --title="Aditya"
+//it calls the handler function if the keyword describle in "command:" is detected in the cmd  
+//for add
+yargs.command({
+    command: 'add',
+    describe: 'Add notes',
+    handler: function (){
+        console.log('adding notes..')
+    }
+})//node app add
+
+//for remove
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function (){
+        console.log('removing note..')
+    }
+})//node app remove --doing so will call the function created inside this yargs command
+
+//for list
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function (){
+        console.log('listing the notes..')
+    }
+})//node app list
+
+//for read
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function (){
+        console.log('reading notes..')
+    }
+})//node app read
+
+console.log(yargs.argv)//use node app --help
